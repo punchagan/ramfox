c58d31a0f89161d151047af6df11db8f10d5d922
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-04-18 22:42:56 +0530

    Notify the native app about the newly opened tab's URL

--- add-on/background.js
+++ add-on/background.js
@@ -6,14 +6,27 @@ var port = browser.runtime.connectNative("ping_pong");
 /*
 Listen for messages from the app.
 */
-port.onMessage.addListener((response) => {
-  console.log("Received: " + response);
+port.onMessage.addListener(response => {
+    console.log("Received: " + response);
 });
 
 /*
 On a click on the browser action, send the app a message.
 */
 browser.browserAction.onClicked.addListener(() => {
-  console.log("Sending:  ping");
-  port.postMessage("ping");
+    console.log("Sending:  ping");
+    port.postMessage("ping");
+});
+
+// Detect the event of opening a new tab and try to open that URL in the default browser
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (
+        changeInfo.status == "loading" &&
+        changeInfo.url &&
+        !tab.pinned &&
+        changeInfo.url.indexOf("http") == 0
+    ) {
+        port.postMessage(JSON.stringify({ url: changeInfo.url }));
+        browser.tabs.remove(tabId);
+    }
 });
